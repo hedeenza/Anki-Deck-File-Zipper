316a29ae8d2ceb443df8dc9a6f09f6a80ac72607
Zach Hedeen <hedeenza@gmail.com>
2026-06-12 08:36:34 -0400

    better command line argument handling with Clap, extracted reading the input files line by line into a vector as a reusable funciton

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,75 @@
 use clap::Parser;
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::time::Instant;
 
 #[derive(Parser)]
+#[command(version, about, long_about = None)]
 struct Cli {
+    /// The file containing Target Language sentences
+    #[arg(short, long)]
     target_language: String,
-    base_language: String,
-    output_file: std::path::PathBuf,
+
+    /// The file containing Native Language sentences
+    #[arg(short, long)]
+    native_language: String,
+
+    /// The name of the Output File
+    #[arg(short, long)]
+    output_file: String,
 }
 
 pub fn main() {
-    let program_start = Instant::now(); // Begin Benchmarking Timer
+    // Begin Benchmarking Timer
+    let program_start = Instant::now();
+
+    // Parse command line arguments
     let args = Cli::parse();
-    let content_target = std::fs::read_to_string(&args.target_language)
-        .expect("Could not read Target Language File (1)");
-    let content_base = std::fs::read_to_string(&args.base_language)
-        .expect("Could not read Base Language File (2)");
-    let output = std::env::args()
-        .nth(3)
-        .expect("No output file provided (3)");
 
-    let mut content_vec = Vec::with_capacity(10_000);
-    let mut base_vec = Vec::with_capacity(10_000);
+    // Create vectors to hold the file contents
+    let target_vec = Vec::with_capacity(20_000);
+    let native_vec = Vec::with_capacity(20_000);
 
-    for line in content_target.lines() {
-        content_vec.push(line);
-    }
+    // Read in the Language files, push their contents to their vectors
+    let target_sentences = read_to_vec(args.target_language, target_vec);
+    let native_sentences = read_to_vec(args.native_language, native_vec);
 
-    for line in content_base.lines() {
-        base_vec.push(line);
-    }
+    // Create the Output file
+    let mut outfile =
+        File::create(args.output_file).expect("[ Error ] - Could not CREATE output file");
 
-    println!("{}", output);
-    let mut outfile = File::create(output).expect("[ Error ] - Could not CREATE output file");
+    // Zip the Target Language and Native Language sentences together
+    let paired: Vec<_> = target_sentences.into_iter().zip(native_sentences).collect();
 
-    let paired: Vec<_> = content_vec.into_iter().zip(base_vec).collect();
+    // Write the zipped sentences to the output file
     for pairing in &paired {
-        let out_lines = format!("{:?}\t{:?}\n", pairing.0, pairing.1);
-        outfile
-            .write_all(out_lines.as_bytes())
-            .expect("[ Error ] - Could not WRITE output file");
+        let out_line = format!("{:?}\t{:?}\n", pairing.0, pairing.1);
+        let _write = writeln!(outfile, "{}", out_line);
     }
 
-    let program_duration = program_start.elapsed(); // Finish Benchmarking Timer
+    // Finish Benchmarking Timer
+    let program_duration = program_start.elapsed();
     println!(
         "{} Lines Successfuly Zipped in {:.2?}",
         &paired.len(),
         program_duration
     );
+}
+
+fn read_to_vec(file_name: String, mut language_vector: Vec<String>) -> Vec<String> {
+    // Read in the Native Language file
+    let file = File::open(file_name).expect("Could not read Language File");
+    let reader = BufReader::new(file);
+    // Push Native Language file contents to vector
+    for line in reader.lines() {
+        match line {
+            Ok(line) => {
+                language_vector.push(line);
+            }
+            Err(err) => {
+                println!("[ ERROR ] : {}", err);
+            }
+        }
+    }
+    language_vector
 }
